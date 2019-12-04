3133e1a7b21cd61ba66199718ad11e4eb691541f
devil9108 <53220135+devil9108@users.noreply.github.com>
2019-12-04 13:13:39 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# dynamic
-Created with CodeSandbox
---
+++ main.js
@@ -0,0 +1,4 @@
+let iframe = document.createElement('iframe');
+console.log(iframe,'pradeep');
+iframe.setAttribute('src',url);
+document.getElementsByClassName('chatWindow')[0].appendChild(iframe);
